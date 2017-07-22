5942a51ca76249279ede5767744faafa8776542d
cmajorsolo <cmajorsolo@gmail.com>
2017-07-22 23:06:34 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='RenReTest',
+      version='1.0',
+      description='Solution for RenRe Test',
+      author='Huali Zhao',
+      packages=['pandans'])
